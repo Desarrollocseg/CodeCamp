fedaf2e4ea7e3706cc1eba6ddde17508e5f9f97a
MISTIK-CRUZ <55566835+MISTICKCRUZ@users.noreply.github.com>
2024-04-24 03:00:24 -0500

    diseño-taller2

--- docs/proyectos/taller2/scripts/aside-script.js
+++ docs/proyectos/taller2/scripts/aside-script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Selecciona el enlace de "Servicios" en la barra de navegación
     var serviciosLink = document.querySelector("nav a[href='#servicios']");
     // Selecciona el elemento que contiene los servicios
-    var servicios = document.getElementById("conserv");
+    var servicios = document.getElementById("servicios");
 
     // Agrega un evento de clic al enlace de "Servicios"
     serviciosLink.addEventListener("click", function(event) {
