7ccb4350babd5f769c454887bd5c38189f659cf9
root <55566835+MISTICKCRUZ@users.noreply.github.com>
2024-09-06 15:47:28 -0500

    desarrollocseg

---
+++ docs/proyectos/desarrollocseg/scripts/navbar-script.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var navbar = document.getElementById("navbar");
+    var logoNavbar = document.getElementById("logo-navbar");
+    var logoNavbarImage = document.getElementById("logo-navbar-image");
+    var logo = document.getElementById("logo");
+    var sticky = navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky");
+        logoNavbar.style.display = "block";
+        logo.style.display = "none";
+        logoNavbarImage.src = logo.src; // Copia el src del logo del encabezado al logo de la barra de navegación
+    } else {
+        navbar.classList.remove("sticky");
+        logoNavbar.style.display = "none";
+        logo.style.display = "block";
+    }
+}
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menuItems = document.getElementById('menu-items');
+    menuItems.style.display = (menuItems.style.display === 'block') ? 'none' : 'block';
+});
