0b0baeeb9a62bd7b43047d6de95d1b401b1ca75b
root <55566835+MISTICKCRUZ@users.noreply.github.com>
2024-09-17 12:15:27 -0500

    barra-de-navegacion-comportamiento-desarrollocseg

--- docs/proyectos/desarrollocseg/scripts/navbar-script.js
+++ docs/proyectos/desarrollocseg/scripts/navbar-script.js
@@ -23,3 +23,14 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
     var menuItems = document.getElementById('menu-items');
     menuItems.style.display = (menuItems.style.display === 'block') ? 'none' : 'block';
 });
+
+// Escuchar el evento de cambio de tamaño de la ventana
+window.addEventListener('resize', function() {
+    var menuItems = document.getElementById('menu-items');
+    // Verificar el ancho de la ventana
+    if (window.innerWidth > 601) { // Cambia este valor según tu punto de ruptura para escritorio
+        menuItems.style.display = 'flex'; // Restablecer a 'flex' en escritorio
+    } else {
+        menuItems.style.display = 'none'; // Ocultar el menú en dispositivos móviles por defecto
+    }
+});
